674313e9cf2cdc2024562d673c536fe1f2515bc3
Sharad Banka <sharadb@MACBOOK.local>
2015-02-18 16:41:41 +0530

    Adding insertion sort function

--- bubble_sort.c
+++ bubble_sort.c
@@ -17,16 +17,21 @@ void buble_sort(int* coinBag,int size)
 			}
 		}
 }
-/*void insertion_sort(int* coinBag,int size)
+void insertion_sort(int* coinBag,int size)
 {
-	int i,j;
-	int *coinBag2;
-	for(i=0;i<size;i++)
+	int i,j,temp;
+	for(i=1;i<size;i++)
 	{
-       
-
-	}
-}*/
+       temp=coinBag[i];
+       j=i-1;
+       while(temp<coinBag[j]&& j>=0)
+       {
+          coinBag[j+1]=coinBag[j];
+          j--;
+       }
+       coinBag[j+1]=temp;
+   }
+}
 int main()
 {
 	int *coinBag;
@@ -34,10 +39,23 @@ int main()
 	srand(time(NULL));
 	for(i=0;i<maxsize;i++)
 		coinBag[i]= pow(1,(rand() % 2)) * (rand() % 9);//populating coinBag with positive and negetive numbers randomly
+	printf("BEFORE SORTING\n");
 	for(i=0;i<maxsize;i++)
 		printf("%d\t", coinBag[i]); //displaying coinBag
         printf("\n");
     buble_sort(coinBag,maxsize);
+    printf("After SORTING\n");
+    for(i=0;i<maxsize;i++)
+		printf("%d\t", coinBag[i]); //displaying coinBag
+        printf("\n");
+    for(i=0;i<maxsize;i++)
+		coinBag[i]= pow(1,(rand() % 2)) * (rand() % 9);//populating coinBag with positive and negetive numbers randomly
+	printf("BEFORE SORTING\n");
+	for(i=0;i<maxsize;i++)
+		printf("%d\t", coinBag[i]); //displaying coinBag
+        printf("\n");
+    insertion_sort(coinBag,maxsize);
+    printf("After SORTING\n");
     for(i=0;i<maxsize;i++)
 		printf("%d\t", coinBag[i]); //displaying coinBag
         printf("\n");
